c0e1d133957cd6f7fab5f209de45904beed5a3eb
Ashley Gaskins <ashley.eddings89@gmail.com>
2020-01-21 21:39:54 -0500

    Assignment 2 added to assign_1

--- lambdata_ash827/__init__.py
+++ lambdata_ash827/__init__.py
@@ -1,2 +1,7 @@
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 my_str = "This is my_str from __init__.py"
+
+#Ones and zeros from the assignment
+
+ONES = pd.Dataframe(np.ones(10))
+ZEROS = pd.Dataframe(np.zeros(20))
--- lambdata_ash827/assign_1.py
+++ lambdata_ash827/assign_1.py
@@ -1,6 +1,26 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+#checking for NaN values
+
+def check_na(dataframe):
+    print(dataframe.shape)
+    print (dataframe.isna().sum())
+
+#change our dataframe to read as df
+
+df = pd.Dataframe(data)
+
+#then make sure to have a date column
+
+df['date'] = pd.to_datetime(df['date'], format='%d/%b/%Y:%H:%M:%S +0000', utc = True)
+
+#Ones and zeros from the assignment
 
 ONES = pd.Dataframe(np.ones(10))
 ZEROS = pd.Dataframe(np.zeros(20))
+
+
 
